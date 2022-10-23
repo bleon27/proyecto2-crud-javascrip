f3ae4f6b8bbdd84c9747e000cc97629d69a2e5ba
bleon27 <bleon27leon@gmail.com>
2022-10-23 00:24:23 -0500

    se agrego bootstrap y el boton de editar

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 window.onload = function () {
     let arregloData = [];
     const btnCrear = document.getElementById('btn-crear');
+    const btnActualizar = document.getElementById('btn-actualizar');
+    const btnCancelar = document.getElementById('btn-cancelar');
+    
     btnCrear.addEventListener('click', event => {
+        let objetoDatos = {
+            nombre: '',
+            usuario: '',
+            correo: '',
+            descripcion: '',
+        }
+
+        let nombre = document.getElementById('nombre').value;
+        let usuario = document.getElementById('usuario').value;
+        let correo = document.getElementById('correo').value;
+        let descripcion = document.getElementById('descripcion').value;
+
+        objetoDatos.nombre = nombre;
+        objetoDatos.usuario = usuario;
+        objetoDatos.correo = correo;
+        objetoDatos.descripcion = descripcion;
+        
+        arregloData.push(objetoDatos);
+        //logica del local storage
+        if (localStorage.getItem('valores') == null) {
+            localStorage.setItem('valores', JSON.stringify(arregloData))
+        } else {
+            let datos = JSON.parse(localStorage.getItem('valores'));
+            datos.push(objetoDatos);
+
+            localStorage.setItem('valores', JSON.stringify(datos));
+        }
+        cargaDatosGuardados();
+    });
+    
+    btnActualizar.addEventListener('click', event => {
         let objetoDatos = {
             nombre: '',
             usuario: '',
@@ -40,7 +74,8 @@ window.onload = function () {
             let datos = JSON.parse(localStorage.getItem('valores'));
 
             id = 1;
-            datos.forEach(element => {
+            
+            datos.forEach(function callback(element, index) {
                 let tr = document.createElement('tr');
                 let td1 = document.createElement('td');
                 let td2 = document.createElement('td');
@@ -48,7 +83,8 @@ window.onload = function () {
                 let td4 = document.createElement('td');
                 let td5 = document.createElement('td');
                 let td6 = document.createElement('td');
-                td6.className = 'd-flex justify-content-center'
+                tr.dataset.index = index;
+                td6.className = 'd-flex justify-content-center';
                 
                 let textId = document.createTextNode(id);
                 let textNombre = document.createTextNode(element.nombre);
@@ -56,17 +92,18 @@ window.onload = function () {
                 let textCorreo = document.createTextNode(element.correo);
                 let textDescripcion = document.createTextNode(element.descripcion);
 
+                //editar item
                 let botonEditar = document.createElement('button');
-                botonEditar.className = 'btn btn-warning bnt-editar';
+                botonEditar.className = 'btn btn-sm mx-1 btn-warning bnt-editar';
                 let textEditar = document.createTextNode('Editar');
                 botonEditar.addEventListener('click', event => {
-                    alert('Editar');
+                    editar(botonEditar);
                 });
                 botonEditar.appendChild(textEditar);
 
+                //eliminar item
                 let botonEliminiar = document.createElement('button');
-                botonEliminiar.className = 'btn btn-danger btn-eliminar';
-
+                botonEliminiar.className = 'btn btn-sm mx-1 btn-danger btn-eliminar';
                 let textEliminar = document.createTextNode('Borrar');
                 botonEliminiar.addEventListener('click', event => {
                     alert('eliminar');
@@ -93,6 +130,15 @@ window.onload = function () {
                 id++;
             });
         }
+    }
+    function editar(botonEditar){
+        let index = botonEditar.parentNode.parentNode.dataset.index;
+        let data = JSON.parse(localStorage.getItem('valores'))[index];
+
+        let nombre = document.getElementById('nombre').value = data.nombre;
+        let usuario = document.getElementById('usuario').value = data.usuario;
+        let correo = document.getElementById('correo').value = data.correo;
+        let descripcion = document.getElementById('descripcion').value = data.descripcion;
     }
     cargaDatosGuardados();
 }
